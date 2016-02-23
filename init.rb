1883f25fa4313a21185ff657b9de2c4cf1926db2
manuabalos <manu.abalos@gmail.com>
2016-02-23 10:05:41 +0100

    Configuración plugin.

---
+++ init.rb
@@ -0,0 +1,9 @@
+Redmine::Plugin.register :redmine_integracion_mantis do
+  name 'Redmine Integracion Mantis plugin'
+  author 'jresinas, mabalos'
+  description 'Plugin de Redmine que permite la integracion entre CSMe y Mantis.'
+  version '0.0.1'
+  author_url 'http://www.emergya.es'
+
+  settings :default => {}, :partial => 'settings/redmine_integracion_mantis'
+end
